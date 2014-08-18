4da91161493c26c9b90be9d61f94254b1b359e65
Ian Blenke <ian@blenke.com>
2014-08-18 15:31:54 -0400

    Fixing JMX support, trying to work around JBAS011592 error, hoping this is not AS7-1859

--- run.py
+++ run.py
@@ -37,6 +37,7 @@ if jmx_port != -1:
         '-Dcom.sun.management.jmxremote.authenticate=false',
         '-Dcom.sun.management.jmxremote.local.only=false',
         '-Dcom.sun.management.jmxremote.ssl=false',
+        '-Djava.util.logging.manager=org.jboss.logmanager.LogManager'
     ]
 
 # Add any auto-discovered details from maestro-ng
